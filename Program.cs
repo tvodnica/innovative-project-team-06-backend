f3afb611130da6e5b9bb19d2f518e7f2b4285096
Tomo Vodnica <tvodnica@racunarstvo.hr>
2024-12-04 11:35:47 +0100

    Created models and database connection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+/*builder.Services.AddDbContext<ApiDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+*/
+builder.Services.AddDbContext<ApiDbContext>(options =>
+    options.UseInMemoryDatabase("TestDb"));
+
+
 
 var app = builder.Build();
 
