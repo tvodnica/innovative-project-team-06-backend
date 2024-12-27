b70f6dc7d70b9e61a98afb62ec54ce662e3488f6
jkele <josip.skrill@gmail.com>
2024-12-27 16:34:34 +0100

    Add cors policy

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "corspolicy",
+        policy =>
+        {
+            policy.WithOrigins("*").AllowAnyOrigin()
+            .AllowAnyMethod().AllowAnyHeader();
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("corspolicy");
 
 app.UseAuthorization();
 
